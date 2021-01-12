a25ab8525bc257c2773d2f2d1fdb0f4022063817
annie-elequin <annie@elequin.io>
2021-01-11 21:09:39 -0600

    add push notification library and get the token

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import 'react-native-url-polyfill/auto';
 import 'react-native-gesture-handler';
 import '@rn-matrix/core/shim';
 
+import './mobile/services/notifications';
+
 import React, {useContext, useEffect} from 'react';
 import {matrix, matrixSdk} from '@rn-matrix/core';
 import {
